a31d74b19db59f0430b11718d828b705ff64212c
Lacey Gibbons <lazyrider@D-10-19-16-19.dhcp4.washington.edu>
2019-01-26 11:16:38 -0800

    adding updated files

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -21,6 +21,7 @@ function addButtons(gifArray, addClassTo) {
   for (var i = 0; i < gifArray.length; i++) {
     var btn = $("<button>"); //loops through and creates buttons for array values
     btn.addClass(addClassTo); //adds a class to all the buttons
+    btn.addClass("btn btn-outline-info");
     btn.attr('data-name', gifArray[i]); //adds a type to the button that is the name of each item in the array
     btn.text(gifArray[i]); //adds the name of each item in the array as text to the button
     $("#button-container").append(btn); //adds each button one after the other in the button container div
@@ -55,18 +56,18 @@ $(document).on("click", ".search-button", function() {
       for (var i = 0; i < results.length; i++) { //loops through the images in the array and grabs information needed and stores that info in variables
 
         var gifDiv = $("<div class='gifs col-md-4'>"); // creating a div for each gif to live
-        var rating = results[i].rating; //grabs the rating from each gif
+        var rating = results[i].rating.toUpperCase(); //grabs the rating from each gif
 
         var p = $("<p>").text("Rating: " + rating); //creates a paragraph tag to hold the rating text
         var animated = results[i].images.fixed_height.url; //grabs the animated version of the gif and stores it in a variable
         var still = results[i].images.fixed_height_still.url; // grabs the still version of the gif and stores it in a variable
         var image = $("<img>"); //creating a variable that will add image tags to the html
 
-        image.attr("src", still); //adding the still image to each image tag
+        image.attr("src", still); //adding the still image to each image tag as default image
         image.attr("data-still", still); //giving the still version of the image a data type
         image.attr("data-animated", animated); //giving the animated version of the image a data type
         image.attr("data-state", "still"); //gives the image a default data type state of still
-        image.addClass("gif-image img-responsive img-thumbnail");
+        image.addClass("gif-image img-responsive img-thumbnail"); //adding bootsrap portfolio thumbnail and responsiveness
         gifDiv.append(image); //adds the image to the gif div above
         gifDiv.append(p); //adds the ratings to the gif div above
         $("#query-gif").append(gifDiv); //adds the gif, one after the other, into the query-gif div
@@ -89,10 +90,10 @@ $(document).on('click', '.gif-image', function() {
 // runs function when submit button is clicked
 $("#add-input").on("click", function(event) {
   event.preventDefault(); // event.preventDefault() prevents the form from trying to submit itself....this is because the user can click enter to submit form
-  var newUserInput = $("#user-input").val().trim(); // grabs the user's input
+  var newUserInput = $("#user-input").val().trim().toLowerCase(); // grabs the user's input
   //checks to see if a user entered anything into the input field...will display error if nothing is in field and enter or submit is pressed
   $(".error").remove();
-  if (newUserInput.length < 1) {
+  if (newUserInput.length < 1) {  //adds an error message if user enters nothing in the input box....keeps user from adding blank boxes
     $('form').append('<p class="error">This field is required</p>');
   } else {
     gifArray.push(newUserInput); //pushed the user's input to the array so a new button can be added
